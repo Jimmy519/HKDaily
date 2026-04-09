b089f921e091b25d7b23a8bce2b3de6593be2862
Jimmy519 <96937359+Jimmy519@users.noreply.github.com>
2026-04-09 21:24:36 +0800

    Update SummarizeNews.py

--- SummarizeNews.py
+++ SummarizeNews.py
@@ -6,22 +6,6 @@ from SummarizeNews_getData_Now import get_Now_hottopics
 from SummarizeNews_getData_Youtube import get_Youtube_hottopics
 from SummarizeNews_getData_RTHK import get_RTHK_hottopics
 
-# #Sidebar for people to choose media
-# with st.sidebar:
-#     st.title("Choose the media you want to view")
-#     selections = {}
-#     for media in rss_url.keys():
-#         # Using the dictionary key as the Streamlit key
-#         selections[media] = st.checkbox(media)
-
-#     for media in url.keys():
-#         selections[media] = st.checkbox(media)    
-
-# if st.sidebar.button("Check All"):
-#     for media in rss_url.keys():
-#         st.session_state[f"chk_{media}"] = True
-#     st.rerun()
-
 # This stretches the app to the full width of the browser
 st.set_page_config(layout="wide")
 
@@ -32,16 +16,18 @@ newsPane, ytPane, gsPane = st.columns(3)
 with newsPane:
     st.title(f"{datetime.today().strftime('%Y-%m-%d')} 發生緊乜事")
     get_HK01_hottopics()
+    st.divider()
     get_Now_hottopics()
+    st.divider()
     get_RTHK_hottopics()
     
 #ytPane
 with ytPane:
-    st.title("其他人睇緊乜")
+    st.title("What Others are Viewing")
     get_Youtube_hottopics()
 
 #gsPane
 with gsPane:
-    st.title("其他人搵緊乜")
+    st.title("What Others are Finding")
     df = get_google_trends()
     st.dataframe(df, width="stretch", hide_index=True)
